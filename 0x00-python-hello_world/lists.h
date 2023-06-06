b1707e5a5815d0126e2f399653acd08c702fb341
Empirestar247 <ejimofor81@gmail.com>
2023-06-05 17:02:02 -0700

    Tasks 0 - 9

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -9,6 +9,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * 
  */
 typedef struct listint_s
 {
@@ -22,4 +23,3 @@ void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
-
