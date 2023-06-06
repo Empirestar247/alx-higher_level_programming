20ffb2fd6835ecedc47bdc2f5360f09983a6f3cf
Empirestar247 <ejimofor81@gmail.com>
2023-06-05 17:56:57 -0700

    Tasks 10 - 13

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+# Write the message to stderr
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+
+# Exit with status code 1
+sys.exit(1)
+
