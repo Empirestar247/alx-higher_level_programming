c40bad5ca2a4b466e60b7d732a21d2a22d84b353
Empirestar247 <ejimofor81@gmail.com>
2023-06-06 17:06:59 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for tens in range(10):
-    for ones in range(tens+1, 10):
-        if tens < 8:
-            print("{:d}{:d}, ".format(tens, ones), end="")
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
         else:
-            print("{:d}{:d}".format(tens, ones) end=", ")
+            print("{}{}".format(digit1, digit2), end=", ")
