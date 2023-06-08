d80280f57e243781ca1eea1919c882a49c904a75
Empirestar247 <ejimofor81@gmail.com>
2023-06-08 12:53:16 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,8 +3,8 @@ if __name__ == "__main__":
     a = 1
     b = 2
 
-    import add_0
+    from add_0 import add
 
-    result = add_0.add(a, b)
+    result = add(a, b)
 
     print("{} + {} = {}".format(a, b, result))
