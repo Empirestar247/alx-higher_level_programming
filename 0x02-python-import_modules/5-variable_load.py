78afe1a98fcd461eb3ac8bc9037c1fd2974c40c2
Empirestar247 <ejimofor81@gmail.com>
2023-06-08 19:45:07 -0700

    5-variable_load.py

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Import and print the value of variable a."""
+
+    # Import the variable a from the file
+    from variable_load_5 import a
+
+    # Print the value of a
+    print(a)
