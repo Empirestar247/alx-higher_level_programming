4d7d1680c1897afb9a55fc44750f462249476c56
Empirestar247 <ejimofor81@gmail.com>
2023-06-08 20:07:31 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
 
-import sys
-from calculator_1 import add, subtract, multiply, divide
-
 if __name__ == "__main__":
-    """Handle basic operations using imported functions."""
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, subtract, multiply, divide
+    import sys
 
     # Check the number of arguments
-    if len(sys.argv) != 4:
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    # Get the arguments
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
-
-    # Perform the operation based on the operator
-    if operator == '+':
-        result = add(a, b)
-    elif operator == '-':
-        result = subtract(a, b)
-    elif operator == '*':
-        result = multiply(a, b)
-    elif operator == '/':
-        result = divide(a, b)
-    else:
+    operators = {"+": add, "-": subtract, "*": multiply, "/": divide}
+    if sys.argv[2] not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    # Print the result
-    print("{} {} {} = {}".format(a, operator, b, result))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    result = operators[sys.argv[2]](a, b)
+    print("{} {} {} = {}".format(a, sys.argv[2], b, result))
