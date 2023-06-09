e1e7203a4548afb629deb3b78d81697dc2008329
Empirestar247 <ejimofor81@gmail.com>
2023-06-08 19:00:35 -0700

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
+
+    total = 0
+    # Iterate through each argument except the first one (which is the script name)
+    for i in range(len(sys.argv) - 1):
+        # Convert the argument to an integer and add it to the total
+        total += int(sys.argv[i + 1])
+
+    # Print the total
+    print("{}".format(total))
