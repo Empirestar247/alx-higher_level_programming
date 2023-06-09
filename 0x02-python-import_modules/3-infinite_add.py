e129a43a5eded9b4b9130904373c8cfb070458f3
Empirestar247 <ejimofor81@gmail.com>
2023-06-08 19:04:38 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     import sys
 
     total = 0
-    # Iterate through each argument except the first one (which is the script name)
+    # Iterate through each argument except the first one
     for i in range(len(sys.argv) - 1):
         # Convert the argument to an integer and add it to the total
         total += int(sys.argv[i + 1])
