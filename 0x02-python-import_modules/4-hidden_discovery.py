6338af41207eab4de3cb2f2c61272893492ba39c
Empirestar247 <ejimofor81@gmail.com>
2023-06-08 19:35:49 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
 
-import importlib.util
-import urllib.request
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
 
-# Specify the URL of the hidden_4.pyc file
-url = "https://github.com/alx-tools/0x02.py/raw/master/hidden_4.pyc"
+    # Import the hidden_4 module
+    import hidden_4
 
-# Download the hidden_4.pyc file
-urllib.request.urlretrieve(url, "hidden_4.pyc")
+    # Get all the names defined in the module
+    module_names = dir(hidden_4)
 
-# Load the compiled module using importlib.util
-spec = importlib.util.spec_from_file_location("hidden_4", "hidden_4.pyc")
-module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(module)
-
-# Get all the names defined in the module
-names = dir(module)
-
-# Filter out names that start with "__"
-filtered_names = [name for name in names if not name.startswith("__")]
-
-# Sort the names alphabetically
-filtered_names.sort()
-
-# Print the names
-for name in filtered_names:
-    print(name)
+    # Iterate through each name
+    for name in module_names:
+        # Check if the name does not start with "__"
+        if not name.startswith("__"):
+            # Print the name
+            print(name)
