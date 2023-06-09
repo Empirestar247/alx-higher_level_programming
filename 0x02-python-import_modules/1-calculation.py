893b5d8541226d2f5f39c3a228eb145864d946f5
Empirestar247 <ejimofor81@gmail.com>
2023-06-08 18:00:22 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
     a = 10
     b = 5
-
-    import calculator_1
-
-    addition_result = calculator_1.add(a, b)
-    subtraction_result = calculator_1.subtract(a, b)
-    multiplication_result = calculator_1.multiply(a, b)
-    division_result = calculator_1.divide(a, b)
-
-    print("Addition: {}".format(addition_result))
-    print("Subtraction: {}".format(subtraction_result))
-    print("Multiplication: {}".format(multiplication_result))
-    print("Division: {}".format(division_result))
+    print('{:d} + {:d} = {:d}'.format(a, b, (add(a, b))))
+    print('{:d} - {:d} = {:d}'.format(a, b, (sub(a, b))))
+    print('{:d} * {:d} = {:d}'.format(a, b, (mul(a, b))))
+    print('{:d} / {:d} = {:d}'.format(a, b, (div(a, b))))
