abdc6146324468a2c1810581fe67faad23f40d5c
Empirestar247 <ejimofor81@gmail.com>
2023-06-08 18:24:07 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-
 import sys
 
-# Get the number of arguments
-num_args = len(sys.argv) - 1
+if __name__ == "__main__":
+    # Get the number of arguments
+    num_args = len(sys.argv) - 1
 
-# Print the number of arguments
-if num_args == 0:
-    print("Number of argument(s): 0.")
-elif num_args == 1:
-    print("Number of argument(s): 1 (argument):")
-else:
-    print("Number of argument(s): {} (arguments):".format(num_args))
+    # Print the number of arguments
+    if num_args == 0:
+        print("Number of argument(s): 0.")
+    elif num_args == 1:
+        print("Number of argument(s): 1 (argument):")
+    else:
+        print("Number of argument(s): {} (arguments):".format(num_args))
 
-# Print the list of arguments
-for i, arg in enumerate(sys.argv[1:], start=1):
-    print("{}: {}".format(i, arg))
+    # Print the list of arguments
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print("{}: {}".format(i, arg))
