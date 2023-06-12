9eb85726f56316351403b28e072360be62e461ea
Empirestar247 <ejimofor81@gmail.com>
2023-06-12 02:22:10 -0700

    0-print_list_integer.py

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    for num in my_list:
+        print("{}".format(num))
+
+# Example usage
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
