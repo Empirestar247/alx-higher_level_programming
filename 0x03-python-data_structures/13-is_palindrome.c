1679dc807ddd471d09c68d4669f57215051e1109
Empirestar247 <ejimofor81@gmail.com>
2023-06-13 06:46:06 -0700

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,83 +1,81 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
-/**
-  * is_palindrome - Checks if a singly linked list is a palindrome
-  * @head: The head of the singly linked list
-  *
-  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
-  */
-int is_palindrome(listint_t **head)
-{
-    listint_t *start = NULL, *end = NULL;
-    unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
+#include <stdio.h>
+#include <stdlib.h>
 
-    if (head == NULL)
-        return (0);
+/* Definition of a node in the linked list */
+typedef struct listint_t {
+    int data;
+    struct listint_t* next;
+} listint_t;
 
-    if (*head == NULL)
-        return (1);
-    
-    start = *head;
-    len = listint_len(start);
-    len_cyc = len * 2;
-    len_list = len_cyc - 2;
-    end = *head;
-
-    for (; i < len_cyc; i = i + 2)
-    {
-        if (start[i].n != end[len_list].n)
-            return (0);
-
-        len_list = len_list - 2;
+/* Function to check if a singly linked list is a palindrome */
+int is_palindrome(listint_t** head) {
+    if (*head == NULL || (*head)->next == NULL) {
+        return 1;  // Empty list or single element list is considered a palindrome
     }
 
-    return (1);
+    // Find the middle of the linked list using the two-pointer technique
+    listint_t* slow = *head;
+    listint_t* fast = *head;
+    while (fast->next != NULL && fast->next->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    // Reverse the second half of the linked list
+    listint_t* prev = NULL;
+    listint_t* current = slow->next;
+    listint_t* next = NULL;
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    // Compare the first half with the reversed second half of the linked list
+    listint_t* first = *head;
+    listint_t* second = prev;
+    while (second != NULL) {
+        if (first->data != second->data) {
+            return 0;  // Not a palindrome
+        }
+        first = first->next;
+        second = second->next;
+    }
+
+    return 1;  // Palindrome
 }
 
-/**
-  * get_nodeint_at_index - Gets a node from a linked list
-  * @head: The head of the linked list
-  * @index: The index to find in the linked list
-  *
-  * Return: The specific node of the linked list
-  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
-{
-	listint_t *current = head;
-	unsigned int iter_times = 0;
+int main() {
+    // Test the is_palindrome function
 
-	if (head)
-	{
-		while (current != NULL)
-		{
-			if (iter_times == index)
-				return (current);
+    // Create a palindrome linked list: 1 -> 2 -> 3 -> 2 -> 1
+    listint_t* head = (listint_t*)malloc(sizeof(listint_t));
+    head->data = 1;
+    head->next = (listint_t*)malloc(sizeof(listint_t));
+    head->next->data = 2;
+    head->next->next = (listint_t*)malloc(sizeof(listint_t));
+    head->next->next->data = 3;
+    head->next->next->next = (listint_t*)malloc(sizeof(listint_t));
+    head->next->next->next->data = 2;
+    head->next->next->next->next = (listint_t*)malloc(sizeof(listint_t));
+    head->next->next->next->next->data = 1;
+    head->next->next->next->next->next = NULL;
 
-			current = current->next;
-			++iter_times;
-		}
-	}
+    int result = is_palindrome(&head);
+    printf("Is palindrome? %d\n", result);  // Output: Is palindrome? 1
 
-	return (NULL);
+    // Free the memory allocated for the linked list
+    listint_t* current = head;
+    listint_t* next = NULL;
+    while (current != NULL) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+
+    return 0;
 }
 
-/**
-  * slistint_len - Counts the number of elements in a linked list
-  * @h: The linked list to count
-  *
-  * Return: Number of elements in the linked list
-  */
-size_t listint_len(const listint_t *h)
-{
-	int lenght = 0;
-
-	while (h != NULL)
-	{
-		++lenght;
-		h = h->next;
-	}
-
-	return (lenght);
-}
