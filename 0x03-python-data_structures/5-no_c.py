23ba02e38ecbd61cef2acfef41a3a5c6891f9aa1
Empirestar247 <ejimofor81@gmail.com>
2023-06-13 02:35:29 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    l = list(my_string)
-    for x in l:
-        if x == 'c':  # Remove lowercase 'c'.
-            l.remove('c')
-        if x == 'C':  # Remove uppercase 'C'.
-            l.remove('C')
-    my_string = "".join(l)
-    return my_string
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
