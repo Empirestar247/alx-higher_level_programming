5e84037483dcdf21ebfe8da7e25f30228ac50aaa
Empirestar247 <ejimofor81@gmail.com>
2023-06-13 00:36:50 -0700

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,9 @@
-#!/usr/bin/pythno3
+#!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return None
-    return my_list[idx]
+    for x in my_list:
+        if idx == my_list.index(x):  # Find element at specified index.
+            return(x)  # Return the element.
+        elif idx > len(my_list):  # If index is out of range, return None.
+            return None
+        elif idx < 0:  # If index is negative, return None.
+            return None
