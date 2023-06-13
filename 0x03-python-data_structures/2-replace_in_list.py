080b38942461ffcadac9d411cfac63afd137c379
Empirestar247 <ejimofor81@gmail.com>
2023-06-13 01:28:35 -0700

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     for x in my_list:
-        if idx == my_list.index(x):  # Find the element at the specified index.
-            my_list.pop(idx)  # Remove the element at the index.
-            my_list.insert(idx, element)  # Insert the new element at the index.
-            return my_list  # Return the updated list.
-        elif idx >= len(my_list):  # If index is out of range, return the original list.
+        if idx == my_list.index(x):  # Replace element at index.
+            my_list.pop(idx)
+            my_list.insert(idx, element)
             return my_list
-        elif idx < 0:  # If index is negative, return the original list.
+        elif idx >= len(my_list):
+            return my_list
+        elif idx < 0:  # Return original list if index is negative.
             return my_list
