e48ad741642153fed7fe2861e8022e087f93cd8a
Empirestar247 <ejimofor81@gmail.com>
2023-06-12 23:44:52 -0700

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,19 +1,4 @@
 #!/usr/bin/python3
-
 def print_list_integer(my_list=[]):
-    """
-    Prints the elements of a list as integers, each on a new line.
-
-    Args:
-        my_list (list): The list of integers to be printed. Defaults to an empty list if not provided.
-
-    Returns:
-        None
-    """
-    if my_list:
-        # Use a list comprehension to convert each element to a string and join them with newlines
-        print('\n'.join(['{:d}'.format(n) for n in my_list]))
-
-# Example usage
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
+    for i in my_list:
+        print("{:d}".format(i))  # Print each integer in the list.
