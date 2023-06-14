d454b135304004664aa39855928d7021854ccfec
Empirestar247 <ejimofor81@gmail.com>
2023-06-14 14:31:09 -0700

    7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    Replaces or adds a key/value in a dictionary.
+    """
+    a_dictionary[key] = value
+    return a_dictionary
