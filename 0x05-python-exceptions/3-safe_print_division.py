37f28a1d39f276cf429b0411ac28e4f17f3ac33f
Empirestar247 <ejimofor81@gmail.com>
2023-06-26 17:30:31 -0700

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def safe_print_division(a, b):
     """
     divides two integers and prints the result
@@ -8,7 +6,7 @@ def safe_print_division(a, b):
     """
     try:
         res = a / b
-    except:
+    except ZeroDivisionError:
         res = None
     finally:
         print("Inside result: {}".format(res))
