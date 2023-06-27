a4f671e3090f5f679534b36f2f275f2267fa0a9a
Empirestar247 <ejimofor81@gmail.com>
2023-06-27 15:35:17 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): The size of the square (private).
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a square instance with a given size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
