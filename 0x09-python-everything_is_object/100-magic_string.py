5d6270232a5ebe898b1d8dc3810262949443d601
Empirestar247 <ejimofor81@gmail.com>
2023-07-05 11:53:42 -0700

    Everything is python

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-
-def magic_string():
-    return "\n".join(["BestSchool" * (i+1) for i in range(5)])
+def magic_string(i=[0]):
+    i[0] += 1
+    return str("BestSchool, " * (i[0] - 1)) + "BestSchool"
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""Defines a locked class that restricts attribute creation."""
+
 
 class LockedClass:
-    __slots__ = ('first_name',)
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
 
-    def __setattr__(self, name, value):
-        if name != 'first_name':
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
-        self.__dict__[name] = value
+    __slots__ = ["first_name"]
