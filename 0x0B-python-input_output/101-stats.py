ac062ffb1edffefbedeef98fdcb347c6103c31b8
Empirestar247 <ejimofor81@gmail.com>
2023-07-11 15:41:25 -0700

    Task 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,39 +1,56 @@
-#sr/bin/python3
-"""Module containing a script that reads stdin line by line"""
+#!/usr/bin/python3
+""" Module to print status code """
+ import sys
 
-import sys
 
-file_size = 0
-status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0}
-i = 0
-try:
-    for line in sys.stdin:
-        tokens = line.split()
-        if len(tokens) >= 2:
-            a = i
-            if tokens[-2] in status_tally:
-                status_tally[tokens[-2]] += 1
-                i += 1
-            try:
-                file_size += int(tokens[-1])
-                if a == i:
-                    i += 1
-            except Exception:
-                if a == i:
-                    continue
-        if i % 10 == 0:
-            print("File size: {:d}".format(file_size))
-            for key, value in sorted(status_tally.items()):
-                if value:
-                    print("{:s}: {:d}".format(key, value))
-    print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
-        if value:
-            print("{:s}: {:d}".format(key, value))
+ class StatusCodeAnalyzer:
+     """ Class to generate instances with dictionary and size"""
+     def __init__(self):
+         """ Init method """
+         self.status_dict = {}
+         self.size = 0
 
-except KeyboardInterrupt:
-    print("File size: {:d}".format(file_size))
-    for key, value in sorted(status_tally.items()):
-        if value:
-            print("{:s}: {:d}".format(key, value))
+     def init_status_dict(self):
+         """ Initialize dictionary """
+         self.status_dict['200'] = 0
+         self.status_dict['301'] = 0
+         self.status_dict['400'] = 0
+         self.status_dict['401'] = 0
+         self.status_dict['403'] = 0
+         self.status_dict['404'] = 0
+         self.status_dict['405'] = 0
+         self.status_dict['500'] = 0
+
+     def add_status_code(self, status):
+         """ Add repeated number to the status code """
+         if status in self.status_dict:
+             self.status_dict[status] += 1
+
+     def print_info(self, sig=0, frame=0):
+         """ Print status code """
+         print("File size: {:d}".format(self.size))
+         for key in sorted(self.status_dict.keys()):
+             if self.status_dict[key] != 0:
+                 print("{}: {:d}".format(key, self.status_dict[key]))
+
+ if __name__ == "__main__":
+     analyzer = StatusCodeAnalyzer()
+     analyzer.init_status_dict()
+     nlines = 0
+
+     try:
+         for line in sys.stdin:
+             if nlines % 10 == 0 and nlines != 0:
+                 analyzer.print_info()
+
+             try:
+                 list_line = [x for x in line.split(" ") if x.strip()]
+                 analyzer.add_status_code(list_line[-2])
+                 analyzer.size += int(list_line[-1].strip("\n"))
+             except:
+                 pass
+             nlines += 1
+     except KeyboardInterrupt:
+         analyzer.print_info(
+         raise
+     analyzer.print_info()
