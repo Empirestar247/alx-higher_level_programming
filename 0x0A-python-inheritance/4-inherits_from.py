55173dbe436216fcc6063444d3141121c9d86688
Empirestar247 <ejimofor81@gmail.com>
2023-07-11 23:47:57 -0700

    Task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,14 +1,13 @@
+#!/usr/bin/python3
 def is_kind_of_class(obj, a_class):
     """
     Define if obj is an instance of a_class or its subclasses.
     Returns True if obj is an instance of a_class or its subclasses,
     False otherwise.
     """
-    # Check if obj is an instance of a_class or its subclasses
     if isinstance(obj, a_class):
         return True
 
-    # Check if obj is an instance of any subclasses of a_class
     for subclass in a_class.__subclasses__():
         if isinstance(obj, subclass):
             return True
