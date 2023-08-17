4723c71f52fc2ce72fe0f1e2125054cb12d0b7d1
Empirestar247 <ejimofor81@gmail.com>
2023-08-16 19:50:14 -0700

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,23 @@
--- creates a user and sets a password
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- Script: Create MySQL User user_0d_1
+-- Description: This script creates a MySQL user with all privileges, named user_0d_1.
+
+-- Check if the user already exists
+SELECT COUNT(*) INTO @user_exists FROM mysql.user WHERE user = 'user_0d_1';
+
+-- Create the user if it doesn't exist
+SET @sql = IF(@user_exists = 0,
+    'CREATE USER "user_0d_1" IDENTIFIED BY "user_0d_1_pwd";',
+    'SELECT "User user_0d_1 already exists";'
+);
+
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO "user_0d_1";
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
+
+-- End of script
