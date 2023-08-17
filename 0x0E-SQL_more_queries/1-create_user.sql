0b84aad8913ab7d2ea89991436fc72e2c7a2e7b3
Empirestar247 <ejimofor81@gmail.com>
2023-08-16 19:37:19 -0700

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,3 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS user_0d_1@localhost
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-    ON *.*
-    TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+-- creates a user and sets a password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
