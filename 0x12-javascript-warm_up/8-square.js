108e753a9900a19c9cf68d7dd72b51baac2aa8c5
Empirestar247 <ejimofor81@gmail.com>
2023-09-13 21:58:17 -0700

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,11 @@
-i#!/usr/bin/node
-const size = process.argv[2];
-if (isNaN(Math.floor(Number(size)))) {
+#!/usr/bin/node
+const size = Math.floor(Number(process.argv[2]));
+if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < size; i++) {
-    console.log('X'.repeat(size));
+  for (let r = 0; r < size; r++) {
+    let row = '';
+    for (let c = 0; c < size; c++) row += 'X';
+    console.log(row);
   }
 }
