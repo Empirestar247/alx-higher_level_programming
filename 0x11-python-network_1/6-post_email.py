21cf0ee46633be4933d1b1d47857f513f8952bb9
Empirestar247 <ejimofor81@gmail.com>
2023-09-29 15:28:26 -0700

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-A Python script that takes in a URL and an email address as command-line arguments,
-sends a POST request to the specified URL with the email as a parameter,
-and displays the body of the response.
+Script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email,
+and displays the body of the response
 """
 import requests
 import sys
