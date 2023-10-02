86c212b6cfbd7ace82176563dd45ce7c46213369
Empirestar247 <ejimofor81@gmail.com>
2023-10-02 12:54:03 -0700

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 """
-Module that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with a letter as a parameter
+Python script that takes in a letter
+and sends a POST request
 """
+from sys import argv
 import requests
-import sys
+
+
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5000/search_user"
+    arg = ""
+    if len(argv) == 2:
+        arg = argv[1]
     try:
-        q = sys.argv[1]
-    except IndexError:
-        q = ""
-    data = {'q': q}
-    r = requests.post(url, data=data)
-    try:
-        id_ = r.json()['id']
-        name_ = r.json()['name']
-        print("[{}] {}".format(id_, name_))
+        r = requests.post('http://0.0.0.0:5000/search_user', data={'q': arg})
+        j = r.json()
+        if j:
+            print('[{}] {}'.format(j.get('id'), j.get('name')))
+        else:
+            print('No result')
     except ValueError:
-        print("Not a Valid Json")
-    except KeyError:
-        print("No result")
+        print('Not a valid JSON')
