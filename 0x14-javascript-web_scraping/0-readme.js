f8d2dd51e973d126ab9e0c9efee1733b364dd34b
Empirestar247 <ejimofor81@gmail.com>
2023-10-25 19:29:09 -0700

    updated

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-const file = process.argv[2];
-fs.readFile(file, 'utf-8', function(error,data) {
-	if(error) {
-		console.log(error);
-	} else {
-		console.log(data);
-	}
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
 });
