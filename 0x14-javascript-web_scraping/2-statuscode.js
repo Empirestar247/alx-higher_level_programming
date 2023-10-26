5991ab193b9fe10deac9c935600a69f68f12d4c4
Empirestar247 <ejimofor81@gmail.com>
2023-10-25 19:31:45 -0700

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = process.argv[2];
-request(url, function (error, response) {
-	if (error) {
-		console.log(error);
-	} else {
-		console.log('code: ' + response.statusCode);
-	}
+
+request(process.argv[2], function (_err, res) {
+  console.log('code:', res.statusCode); // Print the response status code if a response was received
 });
