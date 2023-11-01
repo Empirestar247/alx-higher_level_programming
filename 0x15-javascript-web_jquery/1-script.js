ab1a6fc0a0414077e8c026887588e3ddffbd9d47
Empirestar247 <ejimofor81@gmail.com>
2023-11-01 09:48:05 -0700

    javascript-web_jquery

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,4 @@
+const $ = window.$;
+$(function () {
+  $('header').css({ color: '#FF0000' });
+});
---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+const $ = window.$;
+$(function () {
+  $('DIV#red_header').on('click', function (e) {
+    $('header').css({ color: '#FF0000' });
+  });
+});
